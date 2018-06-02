07518aa9d02e104ebd5d718f8407c1779284c630
OswaldoPeM <39318131+OswaldoPeM@users.noreply.github.com>
2018-06-01 21:27:35 -0500

    Update list.py

--- list.py
+++ list.py
@@ -1,11 +1,11 @@
 myList = [8,12,45,1,16]
 
-print(myList)
-print(myList[0])
-print(len(myList))
+#print(myList)
+#print(myList[0])
+#print(len(myList))
 
-for x in range(len(myList)):
-	print(myList[x])
+#for x in range(len(myList)):
+	#print(myList[x])
 	
 for num in myList:
 	print(num)
