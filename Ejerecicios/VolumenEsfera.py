9a097f77735f318fe395900b98ebee65683af4e5
OswaldoPeM <39318131+OswaldoPeM@users.noreply.github.com>
2018-06-17 20:06:34 -0500

    Add files via upload

--- Ejerecicios/VolumenEsfera.py
+++ Ejerecicios/VolumenEsfera.py
@@ -1,13 +1,14 @@
 '''Escribir una función que calcule el volumen de una esfera.
 La función deberá recibir el valor del radio.'''
 
-def volumen(r):
-	v = (4/3) * 3.1416 * ((r)**3) 
-	print("El volumen de tu esfera es de " + str(v) + str("centimetros"))
+def volumen():
+    r = float(input("Introducir radio de la esfera en centimetros: "))
+    v = (4/3) * 3.1416 * ((r)**3) 
+    print("El volumen de tu esfera es de " + str(v) + str("centimetros"))
 
 def main():
-	x = float(input("Introducir radio de la esfera en centimetros: "))
-	volumen(x)
+	
+	volumen()
 
 if __name__ == "__main__":
 	main()
